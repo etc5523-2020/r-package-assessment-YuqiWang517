a444abaa9a572eeba7b991522e430db1194770c4
“Yuqi <ywan0454@student.monash.edu>
2020-10-23 11:13:59 +0800

    update r documents

---
+++ R/covid1.R
@@ -0,0 +1,7 @@
+#'Comapre the everyday new cases in Australia and Canada
+#'
+#'This is calculated everyday new cases from 01 March 2020 to 01 May 2020 in Australia and Canada 
+#'
+#'@source
+#'WHO
+"covid1"
---
+++ R/covid2.R
@@ -0,0 +1,7 @@
+#'Comapre the total confirmed cases and deaths in Australia and Canada
+#'
+#'This is calculated total cases and deaths from 03 Jan 2020 to 07 Oct 2020 in Australia and Canada 
+#'
+#'@source
+#'WHO
+"covid2"
