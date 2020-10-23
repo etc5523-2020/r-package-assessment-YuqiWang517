3ea1aa63d381d4a141ffeff3361ef8cccdc850e3
“Yuqi <ywan0454@student.monash.edu>
2020-10-23 12:18:20 +0800

    update r documents

--- R/launchapp.R
+++ R/launchapp.R
@@ -1,5 +1,11 @@
-
+#'Shiny for comparing COVID19 cases in Australia and Canada
+#'
+#'This is visualazing and analyzing cases and deaths from 03 Jan 2020 to 07 Oct 2020 in Australia and Canada 
+#'
+#'
 #' @export
+"launch_app"
+
 launch_app <- function() {
   
   appDir <- system.file("app", package = "covidpkg")
